3627d8495e452972182c8bdfc9d2e5cb790ba9d2
tee <semada15@gmail.com>
2020-03-24 17:01:51 +0700

    S

---
+++ src/components/Balance.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Balance(props) {
+  const transactions = props.transactions;
+  const amounts = transactions.map( t => t.amount);
+  const total = amounts.reduce( (sum,amount) => (sum+=amount), 0);
+
+  let sign = total<0 ? '-':'+';
+  if (total===0) {
+    sign = ''
+  }
+
+  return (
+    <div className="container mt-5 text-center">
+      <h4>Your Balance</h4>
+      <h1 id="balance">{sign}฿{Math.abs(total)}</h1>
+    </div>
+
+ 
+  )
+}
---
+++ src/components/IncomeExpense.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default function IncomeExpense(props) {
+
+  const transactions = props.transactions;
+
+  const amounts = transactions.map( transaction => transaction.amount);
+  const income = amounts
+    .filter( item => item>0)
+    .reduce ( (sum, item) => (sum += item),0);
+
+  const expense = -1 * amounts
+    .filter( item => item<0)
+    .reduce ( (sum, item) => (sum += item),0);
+
+
+
+  return (
+    <div className="inc-exp-container">
+      <div>
+        <h4>Income</h4>
+        <p className="money plus">฿{income}</p>
+      </div>
+      <div>
+        <h4>Expense</h4>
+        <p className="money minus">฿{expense}</p>
+      </div>
+    </div>
+
+  )
+}
