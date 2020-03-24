1b7aecf511b6887a25c274511f47de354bd3ca1d
tee <semada15@gmail.com>
2020-03-24 17:14:42 +0700

    Updates

---
+++ precache-manifest.bc401cfed38d7a05c2a61d641467ddf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52e55bcfb99879e288f36ea39d062900",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2edfe06968df53068c41",
+    "url": "/static/css/main.04418a4e.chunk.css"
+  },
+  {
+    "revision": "4a7dbb8c10f1a3b2556c",
+    "url": "/static/js/2.67bcdbb0.chunk.js"
+  },
+  {
+    "revision": "9cc59aa88bbb965336535eb6b360773d",
+    "url": "/static/js/2.67bcdbb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2edfe06968df53068c41",
+    "url": "/static/js/main.babb9e90.chunk.js"
+  },
+  {
+    "revision": "0d3bbb528df5eb49ddfc",
+    "url": "/static/js/runtime-main.caa9dce7.js"
+  }
+]);
